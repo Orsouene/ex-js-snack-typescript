1781e728cc2f6043bd7c8d0c07c93143b5259866
Orsouene <orsoueneelaouizeb@gmail.com>
2025-05-01 14:43:06 +0200

    Snack-1

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
 import './style.css'
+import "./style.css";
+
+const data: unknown = undefined;
+
+if (typeof data === "string") {
+  console.log(data.toUpperCase());
+}
+if (typeof data === "number") {
+  console.log(data * 2 );
+}
+if (typeof data === "boolean") {
+  console.log(data);
+}
+
+else {
+  console.log("Tipo non supportato");
+}
 
 
-
-console.log("orsouene")
